e023fae2bf0fa5f55d087b267535864090c98648
Johan Cwiklinski <johan@x-tnd.be>
2013-09-06 07:50:59 +0200

    Update translations

--- lang/lang_english.php
+++ lang/lang_english.php
@@ -1,5 +1,5 @@
 <?php
-// This file was automatically generated on Tue Aug 20 05:51:32 2013 (GMT)
+// This file was automatically generated on Fri Sep  6 05:50:02 2013 (GMT)
 // Don't modify it by hand, rather use the target lang from the Makefile.
 
 
@@ -9,19 +9,22 @@ $lang['I live here!'] = 'I live here!';
 // ../templates/default/mymap.tpl:72 
 $lang['Your coordinates has been removed'] = 'Your coordinates has been removed';
 
-// ../templates/default/common_scripts.tpl:99 
+// ../templates/default/common_scripts.tpl:150 
 $lang['Show me where I am'] = 'Show me where I am';
 
 // ../maps_preferences.php:57 
 $lang['Maps Settings'] = 'Maps Settings';
 
-// ../maps.php:74 ../mymap.php:88 ../templates/default/menu.tpl:2 ../templates/default/menu.tpl:7 
+// ../templates/default/common_scripts.tpl:6 ../templates/default/common_scripts.tpl:9 
+$lang['Member'] = 'Member';
+
+// ../maps.php:74 ../mymap.php:86 ../templates/default/menu.tpl:2 ../templates/default/menu.tpl:7 
 $lang['Maps'] = 'Maps';
 
 // ../templates/default/menu.tpl:5 
 $lang['My localization'] = 'My localization';
 
-// ../templates/default/common_scripts.tpl:88 
+// ../templates/default/common_scripts.tpl:133 
 $lang['Search your town...'] = 'Search your town...';
 
 // ../templates/default/mymap.tpl:56 
@@ -33,23 +36,32 @@ $lang['Select your town.'] = 'Select your town.';
 // ../templates/default/mymap.tpl:4 
 $lang['Choose your location'] = 'Choose your location';
 
-// ../ajax_ilivehere.php:83 
+// ../ajax_ilivehere.php:82 
 $lang['Coordinates has not been stored :('] = 'Coordinates has not been stored :(';
 
-// ../templates/default/common_scripts.tpl:108 
+// ../templates/default/common_scripts.tpl:159 
 $lang['Imagery ©'] = 'Imagery ©';
 
-// ../templates/default/common_scripts.tpl:90 
+// ../templates/default/common_scripts.tpl:143 
+$lang['Show legend'] = 'Show legend';
+
+// ../templates/default/common_scripts.tpl:135 
 $lang['Search a town...'] = 'Search a town...';
 
-// ../templates/default/common_scripts.tpl:71 
+// ../templates/default/common_scripts.tpl:1 ../templates/default/common_scripts.tpl:2 
+$lang['Legend'] = 'Legend';
+
+// ../templates/default/common_scripts.tpl:108 
 $lang['An error occured during \'I live here\' process :('] = 'An error occured during \'I live here\' process :(';
+
+// ../templates/default/common_scripts.tpl:159 
+$lang['Map data ©'] = 'Map data ©';
+
+// ../templates/default/common_scripts.tpl:22 ../templates/default/common_scripts.tpl:25 
+$lang['Search result'] = 'Search result';
 
 // ../templates/default/mymap.tpl:61 ../templates/default/mymap.tpl:94 
 $lang['Remove'] = 'Remove';
-
-// ../templates/default/common_scripts.tpl:108 
-$lang['Map data ©'] = 'Map data ©';
 
 // ../templates/default/menu.tpl:9 
 $lang['Maps Preferences'] = 'Maps Preferences';
@@ -57,13 +69,16 @@ $lang['Maps Preferences'] = 'Maps Preferences';
 // ../templates/default/mymap.tpl:39 
 $lang['You clicked at %p'] = 'You clicked at %p';
 
-// ../ajax_ilivehere.php:81 
+// ../ajax_ilivehere.php:80 
 $lang['New coordinates has been stored!'] = 'New coordinates has been stored!';
+
+// ../templates/default/common_scripts.tpl:14 ../templates/default/common_scripts.tpl:17 
+$lang['Member (company)'] = 'Member (company)';
 
 // ../templates/default/mymap.tpl:56 
 $lang['Remove my coordinates'] = 'Remove my coordinates';
 
-// ../templates/default/common_scripts.tpl:108 
+// ../templates/default/common_scripts.tpl:159 
 $lang['OpenStreetMap contributors'] = 'OpenStreetMap contributors';
 
 // ../templates/default/mymap.tpl:77 ../templates/default/mymap.tpl:81 
@@ -72,13 +87,13 @@ $lang['An error occured removing your coordinates :('] = 'An error occured remov
 // ../templates/default/mymap.tpl:85 
 $lang['Cancel'] = 'Cancel';
 
-// ../templates/default/maps.tpl:10 
+// ../templates/default/maps.tpl:15 
 $lang['aka'] = 'aka';
 
-// ../templates/default/common_scripts.tpl:92 
+// ../templates/default/common_scripts.tpl:137 
 $lang['Sorry, that town could not be found.'] = 'Sorry, that town could not be found.';
 
-// ../templates/default/common_scripts.tpl:101 
+// ../templates/default/common_scripts.tpl:152 
 $lang['You seem located outside the boundaries of the map'] = 'You seem located outside the boundaries of the map';
 
 ?>
--- lang/lang_french.php
+++ lang/lang_french.php
@@ -1,5 +1,5 @@
 <?php
-// This file was automatically generated on Tue Aug 20 05:51:32 2013 (GMT)
+// This file was automatically generated on Fri Sep  6 05:50:02 2013 (GMT)
 // Don't modify it by hand, rather use the target lang from the Makefile.
 
 
@@ -9,19 +9,22 @@ $lang['I live here!'] = 'J\'y habite !';
 // ../templates/default/mymap.tpl:72 
 $lang['Your coordinates has been removed'] = 'Vos coordonnées ont été supprimées';
 
-// ../templates/default/common_scripts.tpl:99 
+// ../templates/default/common_scripts.tpl:150 
 $lang['Show me where I am'] = 'Me montrer où je suis';
 
 // ../maps_preferences.php:57 
 $lang['Maps Settings'] = 'Préférences des Cartes';
 
-// ../maps.php:74 ../mymap.php:88 ../templates/default/menu.tpl:2 ../templates/default/menu.tpl:7 
+// ../templates/default/common_scripts.tpl:6 ../templates/default/common_scripts.tpl:9 
+$lang['Member'] = 'Adhérent';
+
+// ../maps.php:74 ../mymap.php:86 ../templates/default/menu.tpl:2 ../templates/default/menu.tpl:7 
 $lang['Maps'] = 'Cartes';
 
 // ../templates/default/menu.tpl:5 
 $lang['My localization'] = 'Ma localisation';
 
-// ../templates/default/common_scripts.tpl:88 
+// ../templates/default/common_scripts.tpl:133 
 $lang['Search your town...'] = 'Chercher votre ville...';
 
 // ../templates/default/mymap.tpl:56 
@@ -33,23 +36,32 @@ $lang['Select your town.'] = 'Sélectionnez votre ville.';
 // ../templates/default/mymap.tpl:4 
 $lang['Choose your location'] = 'Choisissez votre localisation';
 
-// ../ajax_ilivehere.php:83 
+// ../ajax_ilivehere.php:82 
 $lang['Coordinates has not been stored :('] = 'Les coordonnées n\'ont pas été enregistrées :(';
 
-// ../templates/default/common_scripts.tpl:108 
+// ../templates/default/common_scripts.tpl:159 
 $lang['Imagery ©'] = '© Imagerie';
 
-// ../templates/default/common_scripts.tpl:90 
+// ../templates/default/common_scripts.tpl:143 
+$lang['Show legend'] = 'Afficher la légende';
+
+// ../templates/default/common_scripts.tpl:135 
 $lang['Search a town...'] = 'Chercher une ville...';
 
-// ../templates/default/common_scripts.tpl:71 
+// ../templates/default/common_scripts.tpl:1 ../templates/default/common_scripts.tpl:2 
+$lang['Legend'] = 'Légende';
+
+// ../templates/default/common_scripts.tpl:108 
 $lang['An error occured during \'I live here\' process :('] = 'Une erreur est survenue pendant le processus « J\'y habite » :(';
+
+// ../templates/default/common_scripts.tpl:159 
+$lang['Map data ©'] = '© Données cartographiques';
+
+// ../templates/default/common_scripts.tpl:22 ../templates/default/common_scripts.tpl:25 
+$lang['Search result'] = 'Résultat de recherche';
 
 // ../templates/default/mymap.tpl:61 ../templates/default/mymap.tpl:94 
 $lang['Remove'] = 'Supprimer';
-
-// ../templates/default/common_scripts.tpl:108 
-$lang['Map data ©'] = '© Données cartographiques';
 
 // ../templates/default/menu.tpl:9 
 $lang['Maps Preferences'] = 'Préférences des cartes';
@@ -57,13 +69,16 @@ $lang['Maps Preferences'] = 'Préférences des cartes';
 // ../templates/default/mymap.tpl:39 
 $lang['You clicked at %p'] = 'Vous avez cliqué à %p';
 
-// ../ajax_ilivehere.php:81 
+// ../ajax_ilivehere.php:80 
 $lang['New coordinates has been stored!'] = 'Les nouvelles coordonnées ont été enregistrées !';
+
+// ../templates/default/common_scripts.tpl:14 ../templates/default/common_scripts.tpl:17 
+$lang['Member (company)'] = 'Adhérent (personne morale)';
 
 // ../templates/default/mymap.tpl:56 
 $lang['Remove my coordinates'] = 'Supprimer mes coordonnées';
 
-// ../templates/default/common_scripts.tpl:108 
+// ../templates/default/common_scripts.tpl:159 
 $lang['OpenStreetMap contributors'] = 'Contributeurs OpenStreetMap';
 
 // ../templates/default/mymap.tpl:77 ../templates/default/mymap.tpl:81 
@@ -72,13 +87,13 @@ $lang['An error occured removing your coordinates :('] = 'Une erreur est survenu
 // ../templates/default/mymap.tpl:85 
 $lang['Cancel'] = 'Annuler';
 
-// ../templates/default/maps.tpl:10 
+// ../templates/default/maps.tpl:15 
 $lang['aka'] = 'aka';
 
-// ../templates/default/common_scripts.tpl:92 
+// ../templates/default/common_scripts.tpl:137 
 $lang['Sorry, that town could not be found.'] = 'Désolé, cette ville n\'a pas été trouvée.';
 
-// ../templates/default/common_scripts.tpl:101 
+// ../templates/default/common_scripts.tpl:152 
 $lang['You seem located outside the boundaries of the map'] = 'Vous semblez être localisé en dehors de limites de la carte';
 
 ?>
