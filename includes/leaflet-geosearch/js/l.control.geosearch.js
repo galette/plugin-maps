9d73f539105a9ea98db0dd03baa158ae3d0f1df0
Johan Cwiklinski <johan@x-tnd.be>
2013-12-15 10:14:57 +0100

    Update to latest geosearch plugin

--- includes/leaflet-geosearch/js/l.control.geosearch.js
+++ includes/leaflet-geosearch/js/l.control.geosearch.js
@@ -1,13 +1,10 @@
 /*
- * L.Control.GeoSearch - search for an address and zoom to it's location
+ * L.Control.GeoSearch - search for an address and zoom to its location
  * https://github.com/smeijer/leaflet.control.geosearch
  */
 
 L.GeoSearch = {};
 L.GeoSearch.Provider = {};
-
-// MSIE needs cors support
-jQuery.support.cors = true;
 
 L.GeoSearch.Result = function (x, y, label) {
     this.X = x;
@@ -17,33 +14,41 @@ L.GeoSearch.Result = function (x, y, label) {
 
 L.Control.GeoSearch = L.Control.extend({
     options: {
-        position: 'topcenter'
+        position: 'topcenter',
+        showMarker: true
+    },
+
+    _config: {
+        country: '',
+        searchLabel: 'search for address ...',
+        notFoundMessage: 'Sorry, that address could not be found.',
+        messageHideDelay: 3000,
+        zoomLevel: 18
     },
 
     initialize: function (options) {
-        this._config = {};
         L.Util.extend(this.options, options);
-        this.setConfig(options);
-    },
-
-    setConfig: function (options) {
-        this._config = {
-            'country': options.country || '',
-            'provider': options.provider,
-            
-            'searchLabel': options.searchLabel || 'search for address...',
-            'notFoundMessage' : options.notFoundMessage || 'Sorry, that address could not be found.',
-            'messageHideDelay': options.messageHideDelay || 3000,
-            'zoomLevel': options.zoomLevel || 18
-        };
+        L.Util.extend(this._config, options);
     },
 
     onAdd: function (map) {
-        var $controlContainer = $(map._controlContainer);
+        var $controlContainer = map._controlContainer,
+            nodes = $controlContainer.childNodes,
+            topCenter = false;
 
-        if ($controlContainer.children('.leaflet-top.leaflet-center').length == 0) {
-            $controlContainer.append('<div class="leaflet-top leaflet-center"></div>');
-            map._controlCorners.topcenter = $controlContainer.children('.leaflet-top.leaflet-center').first()[0];
+        for (var i = 0, len = nodes.length; i < len; i++) {
+            var klass = nodes[i].className;
+            if (/leaflet-top/.test(klass) && /leaflet-center/.test(klass)) {
+                topCenter = true;
+                break;
+            }
+        }
+
+        if (!topCenter) {
+            var tc = document.createElement('div');
+            tc.className += 'leaflet-top leaflet-center';
+            $controlContainer.appendChild(tc);
+            map._controlCorners.topcenter = tc;
         }
 
         this._map = map;
@@ -64,12 +69,13 @@ L.Control.GeoSearch = L.Control.extend({
         resultslist.id = 'leaflet-control-geosearch-results';
         this._resultslist = resultslist;
 
-        $(this._msgbox).append(this._resultslist);
-        $(this._container).append(this._searchbox, this._msgbox);
+        this._msgbox.appendChild(this._resultslist);
+        this._container.appendChild(this._searchbox);
+        this._container.appendChild(this._msgbox);
 
         L.DomEvent
           .addListener(this._container, 'click', L.DomEvent.stop)
-          .addListener(this._container, 'keypress', this._onKeyUp, this);
+          .addListener(this._searchbox, 'keypress', this._onKeyUp, this);
 
         L.DomEvent.disableClickPropagation(this._container);
 
@@ -87,58 +93,120 @@ L.Control.GeoSearch = L.Control.extend({
             }
             else {
                 var url = provider.GetServiceUrl(qry);
-
-                $.getJSON(url, function (data) {
-                    try {
-                        var results = provider.ParseJSON(data);
-                        this._processResults(results);
+                this.sendRequest(provider, url);
+            }
                     }
                     catch (error) {
                         this._printError(error);
                     }
-                }.bind(this));
+    },
+
+    sendRequest: function (provider, url) {
+        var that = this;
+
+        window.parseLocation = function (response) {
+            var results = provider.ParseJSON(response);
+            that._processResults(results);
+
+            document.body.removeChild(document.getElementById('getJsonP'));
+            delete window.parseLocation;
+        };
+
+        function getJsonP (url) {
+            url = url + '&callback=parseLocation'
+            var script = document.createElement('script');
+            script.id = 'getJsonP';
+            script.src = url;
+            script.async = true;
+            document.body.appendChild(script);
             }
+
+        if (XMLHttpRequest) {
+            var xhr = new XMLHttpRequest();
+
+            if ('withCredentials' in xhr) {
+                var xhr = new XMLHttpRequest();
+
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState == 4) {
+                        if (xhr.status == 200) {
+                            var response = JSON.parse(xhr.responseText),
+                                results = provider.ParseJSON(response);
+
+                            that._processResults(results);
+                        } else if (xhr.status == 0 || xhr.status == 400) {
+                            getJsonP(url);
+                        } else {
+                            that._printError(xhr.responseText);
         }
-        catch (error) {
-            this._printError(error);
+                    }
+                };
+
+                xhr.open('GET', url, true);
+                xhr.send();
+            } else if (XDomainRequest) {
+                var xdr = new XDomainRequest();
+
+                xdr.onerror = function (err) {
+                    that._printError(err);
+                };
+
+                xdr.onload = function () {
+                    var response = JSON.parse(xdr.responseText),
+                        results = provider.ParseJSON(response);
+
+                    that._processResults(results);
+                };
+
+                xdr.open('GET', url);
+                xdr.send();
+            } else {
+                getJsonP(url);
+            }
         }
     },
 
     _processResults: function(results) {
-        if (results.length == 0)
-            throw this._config.notFoundMessage;
-
+        if (results.length > 0) {
         this._map.fireEvent('geosearch_foundlocations', {Locations: results});
         this._showLocation(results[0]);
+        } else {
+            this._printError(this._config.notFoundMessage);
+        }
     },
 
     _showLocation: function (location) {
+        if (this.options.showMarker == true) {
         if (typeof this._positionMarker === 'undefined')
             this._positionMarker = L.marker([location.Y, location.X]).addTo(this._map);
         else
             this._positionMarker.setLatLng([location.Y, location.X]);
+        }
 
         this._map.setView([location.Y, location.X], this._config.zoomLevel, false);
         this._map.fireEvent('geosearch_showlocation', {Location: location});
     },
 
     _printError: function(message) {
-        $(this._resultslist)
-            .html('<li>'+message+'</li>')
-            .fadeIn('slow').delay(this._config.messageHideDelay).fadeOut('slow',
-                    function () { $(this).html(''); });
+        var elem = this._resultslist;
+        elem.innerHTML = '<li>' + message + '</li>';
+        elem.style.display = 'block';
+
+        setTimeout(function () {
+            elem.style.display = 'none';
+        }, 3000);
     },
     
     _onKeyUp: function (e) {
-        var escapeKey = 27;
-        var enterKey = 13;
+        var esc = 27,
+            enter = 13,
+            queryBox = document.getElementById('leaflet-control-geosearch-qry');
 
-        if (e.keyCode === escapeKey) {
-            $('#leaflet-control-geosearch-qry').val('');
-            $(this._map._container).focus();
-        }
-        else if (e.keyCode === enterKey) {
-            this.geosearch($('#leaflet-control-geosearch-qry').val());
+        if (e.keyCode === esc) { // escape key detection is unreliable
+            queryBox.value = '';
+            this._map._container.focus();
+        } else if (e.keyCode === enter) {
+            this.geosearch(queryBox.value);
         }
     }
 });
