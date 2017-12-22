1f2377abe1b7c90ce87fd023b18e0bec471dec5a
Johan Cwiklinski <johan@x-tnd.be>
2017-12-22 08:32:11 +0100

    Update locales

--- lang/maps_routes_fr_FR.utf8.php
+++ lang/maps_routes_fr_FR.utf8.php
@@ -1,17 +1,17 @@
 <?php
-// This file was automatically generated on Fri Nov 25 06:24:54 2016 (GMT)
+// This file was automatically generated on Fri Dec 22 07:31:46 2017 (GMT)
 // Don't modify it by hand, rather use the target lang from the Makefile.
 
 
-// ../_routes.php:54 
+// ../_routes.php:47 
 $lang['maps_routes']['/localize-member'] = '/localisation-adherent';
 
-// ../_routes.php:240 
+// ../_routes.php:188 
 $lang['maps_routes']['/i-live-here'] = '/j-y-habite';
 
-// ../_routes.php:158 
+// ../_routes.php:129 
 $lang['maps_routes']['/mymap'] = '/macarte';
 
-// ../_routes.php:174 
+// ../_routes.php:145 
 $lang['maps_routes']['/map'] = '/carte';
 
